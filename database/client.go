5c2a2683507f7db5ccf06eb047a18f72ca01e3d7
Johannes Bandgren <johannes.bandgren@integrio.se>
2022-09-06 08:49:52 +0200

    db

--- database/client.go
+++ database/client.go
@@ -1,5 +1,25 @@
 package database
 
 import (
-	"classified-ads/entities"
+	"github.com/bandgren/classified-ads/entities"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
 )
+
+var Instance *gorm.DB
+var err error
+
+func Connect(connectionString string) {
+	Instance, err = gorm.Open(mysql.Open(connectionString), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+		panic("Cannot connect to DB")
+	}
+	log.Println("Connected to Database...")
+}
+
+func Migrate() {
+	Instance.AutoMigrate(&entities.Ad{})
+	log.Println("Database Migration Completed...")
+}
